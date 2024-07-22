6fb226606202b91734f66dafbef8eb4232c0cb92
matthewburrell-92 <alwayssunny18.20@gmail.com>
2024-07-21 20:47:28 -0600

    Updated Foundation 1

--- final/Foundation1/comment.cs
+++ final/Foundation1/comment.cs
@@ -5,6 +5,11 @@ public class Comment
     public string _author;
     public string _text;
 
+    public Comment(string author, string text)
+    {
+        _author = author;
+        _text = text;
+    }
     public void DisplayComment()
     {
         Console.WriteLine($"{_author} wrote: {_text}");
